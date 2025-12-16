d145e908242aa07a35b164677116cad2250d3905
Mose Schmiedel <mose.schmiedel@stud.htwk-leipzig.de>
2025-12-16 13:44:28 +0100

    Add CUDA support to test script

--- with_sam/scripts/test_sam.py
+++ with_sam/scripts/test_sam.py
@@ -1,8 +1,15 @@
 import torch
 from segment_anything import sam_model_registry
 
-device = "mps" if torch.backends.mps.is_available() else "cpu"
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+
 print(f"MPS available: {torch.backends.mps.is_available()}")
+print(f"CUDA available: {torch.cuda.is_available()}")
 print(f"Using device: {device}")
 
 sam = sam_model_registry["vit_h"](checkpoint="models/sam/sam_vit_h_4b8939.pth")
