cd2661a7db9e22677a6a5432125370b80fc2feb5
Mose Schmiedel <mose.schmiedel@stud.htwk-leipzig.de>
2026-02-12 21:34:20 +0100

    chore: enable gdal exceptions

--- satellit_sam/src/satellit_sam/core/inventory.py
+++ satellit_sam/src/satellit_sam/core/inventory.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from typing import Literal
 
 from osgeo import gdal, ogr, osr
+
+gdal.UseExceptions()
 from pyproj import Geod
 
 from src.satellit_sam.core.allometry import DbhUnit, to_dbh_cm
